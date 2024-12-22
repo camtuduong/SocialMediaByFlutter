f773da1f260ba7a3599c1677834688a347c2caa4
Dương Thị Cẩm Tú <camtu28082003@gmail.com>
2024-12-22 10:00:52 +0700

    Authentication

---
+++ lib/features/auth/presentation/cubits/auth.cubit.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialmediaapp/features/auth/domain/entities/app_user.dart';
+import 'package:socialmediaapp/features/auth/domain/repos/auth_repo.dart';
+import 'package:socialmediaapp/features/auth/presentation/cubits/auth.states.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  final AuthRepo authRepo;
+  AppUser? _currentUser;
+
+  AuthCubit({required this.authRepo}) : super(AuthInitial());
+
+  //check if user is already Authenticated
+  void checkAuth() async {
+    final AppUser? user = await authRepo.getCurrentUser();
+
+    if (user != null) {
+      _currentUser = user;
+      emit(Authenticated(user));
+    } else {
+      emit(Unauthenticated());
+    }
+  }
+
+  //get current user
+  AppUser? get currentUser => _currentUser;
+
+  //login with email + password
+  Future<void> login(String email, String password) async {
+    try {
+      emit(AuthLoading());
+      final user = await authRepo.loginWithEmailPassword(email, password);
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(Unauthenticated());
+      }
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+  //register with email + password
+  Future<void> register(String name, String email, String password) async {
+    try {
+      emit(AuthLoading());
+      final user =
+          await authRepo.registerWithEmailPassword(name, email, password);
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(Unauthenticated());
+      }
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+  //logout
+  Future<void> logout() async {
+    authRepo.logout();
+    emit(Unauthenticated());
+  }
+}
