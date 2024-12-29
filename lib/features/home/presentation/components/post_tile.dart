b3531adfea71268b860e92b66e84dd55f3e59fe2
Dương Thị Cẩm Tú <camtu28082003@gmail.com>
2024-12-29 16:35:36 +0700

    update arlet

--- lib/features/home/presentation/components/post_tile.dart
+++ lib/features/home/presentation/components/post_tile.dart
@@ -1,12 +1,76 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart';
+import 'package:socialmediaapp/features/auth/domain/entities/app_user.dart';
+import 'package:socialmediaapp/features/auth/presentation/cubits/auth_cubit.dart';
 import 'package:socialmediaapp/features/post/domain/entities/post.dart';
+import 'package:socialmediaapp/features/post/presentation/cubits/post_cubit.dart';
+import 'package:socialmediaapp/features/profile/domain/entities/profile_user.dart';
+import 'package:socialmediaapp/features/profile/presentation/cubits/profile_cubit.dart';
 
-class PostTile extends StatelessWidget {
+class PostTile extends StatefulWidget {
   final Post post;
   final void Function()? onDeletePressed;
   const PostTile(
       {super.key, required this.post, required this.onDeletePressed});
+
+  @override
+  State<PostTile> createState() => _PostTileState();
+}
+
+class _PostTileState extends State<PostTile> {
+  //cubits
+  late final postCubit = context.read<PostCubit>();
+  late final profileCubit = context.read<ProfileCubit>();
+
+  bool isOwnPost = false;
+
+  //current user
+  AppUser? currentUser;
+
+  //post user
+  ProfileUser? postUser;
+
+  //on startup
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+    fetchPostUser();
+  }
+
+  void getCurrentUser() {
+    final authCubit = context.read<AuthCubit>();
+    currentUser = authCubit.currentUser;
+    isOwnPost = (widget.post.userId == currentUser!.uid);
+  }
+
+  Future<void> fetchPostUser() async {}
+  //show options for deletion
+  void showOptions() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Delete Post?"),
+        actions: [
+          //cancel btn
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Cancel")),
+
+          //delete btn
+          TextButton(
+              onPressed: () {
+                widget.onDeletePressed!();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Delete")),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +80,11 @@ class PostTile extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             //name
-            Text(post.userName),
+            Text(widget.post.userName),
 
             //delete btn
             IconButton(
-              onPressed: onDeletePressed,
+              onPressed: showOptions,
               icon: const Icon(Icons.delete),
             ),
           ],
@@ -28,7 +92,7 @@ class PostTile extends StatelessWidget {
 
         //image
         CachedNetworkImage(
-          imageUrl: post.imageUrl,
+          imageUrl: widget.post.imageUrl,
           height: 430,
           width: double.infinity,
           fit: BoxFit.cover,
